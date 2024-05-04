dfec8eea96a4328d829e7fcece58489256e4f32a
Anuhya Samudrala <sgn.anuhya@gmail.com>
2024-05-04 18:48:13 -0400

    adding react-vite code

---
+++ frontend/pb/src/component/Login/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Perform login logic here
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+            <div>
+                <a href="#">Sign Up</a>
+            </div>
+            <div>
+                <a href="#">Forgot Password?</a>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
